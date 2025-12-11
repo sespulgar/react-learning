45772942f65be092fbc719376eaaaa8636d28c25
suzie <143464012+sespulgar@users.noreply.github.com>
2025-12-11 16:33:29 +0800

    Add files via upload

---
+++ src/pages/PartOne.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+const PartOne = () => {
+  return (
+    <section className='bg-indigo-50'>
+        <Card>
+        <div>PartOne</div>
+        </Card>
+    </section>
+  )
+}
+
+export default PartOne
